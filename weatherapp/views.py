c1cadefecb45721461033b03674279419bc2ff11
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-09-17 21:14:10 +0300

    weather info on the page

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from decouple import config
+import requests
+from pprint import pprint
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'weatherapp/index.html')
+    API_KEY=config("API_KEY")
+    city = "İstanbul"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+    content = response.json()
+    # pprint(content)
+
+    pprint(content["name"])
+    pprint(content["main"]["temp"])
+    pprint(content["weather"][0]["description"])
+    pprint(content["weather"][0]["icon"])
+
+    context = {
+        "city": content["name"],
+        "temp": content["main"]["temp"],
+        "icon": content["weather"][0]["icon"],
+        "desc": content["weather"][0]["description"]
+    }
+
+
+    return render(request, 'weatherapp/index.html', context)
